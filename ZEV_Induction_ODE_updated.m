2dd43de7e4adbcce1589c94cdef784db928cdfbb
Delaney M. Dow <56002316+delaneydow@users.noreply.github.com>
2022-05-02 00:03:31 -0400

    initial commit containing all files used

---
+++ ZEV_Induction_ODE_updated.m
@@ -0,0 +1,39 @@
+function dxdt = ZEV_Induction_ODE_updated(t,x,time,Est,Ald,kact,kdeg1,kdeg2,kdeg3,nH_Est, nH_Ald, nH_het, ec501, ec502, ec503, kobs)
+
+%% Interpolate Estradiol dose onto time vector
+EstI = interp1(time,Est,t);
+
+%% interpolate aldosterone dose onto time vector
+AldI = interp1(time,Ald,t);
+
+%% PROTEINS
+AA_conc = x(1);          % Cytosolic ZEV
+BB_conc = x(2);           % Nuclear ZEV
+MKATE2 = x(3);               % GFP
+dxdt = zeros(3,1);        % ODE column Vector
+
+%% PARAMETERS
+% nH_Est    = Hill Coefficient for estradiol
+% nH_Ald    = Hill Coefficient for aldosterone
+% nH_het    = Hill Coefficient for heterodimer
+% kact      = Transcriptional activation rate
+% kdeg1     = Degredation rate of AA
+% kdeg2     = Degredation rate of BB
+% kdeg3     = Degredation rate of mKate
+% ec501     = Half maximal effective concentration for estradiol
+% ec502     = Half maximal effective concentration for aldosterone
+% ec503     = Half maximal effective concentration for heterodimer
+% kobs      = Exchange rate homo-/hetero-dimer
+
+%% Equations -- NEED TO MODIFY 
+
+% d[AA]/dt estradiol
+dxdt(1) = kact*[EstI]^nH_Est/([EstI]^nH_Est + ec501^nH_Est) - kdeg1*[AA_conc]; 
+
+% d[BB]/dt aldosterone
+dxdt(2) = kact*[AldI]^nH_Ald/([AldI]^nH_Ald + ec502^nH_Ald) - kdeg2*[BB_conc];
+
+%d[mKate]/dt                    (AB_conc = (AA_conc.*BB_conc./kobs).^0.5)
+dxdt(3) = kact*[(AA_conc.*BB_conc./kobs).^0.5]^nH_het/([(AA_conc.*BB_conc./kobs).^0.5]^nH_het + ec503^nH_het) - kdeg3*[MKATE2];
+
+end
---
+++ messym_Hannah1.m
@@ -0,0 +1,156 @@
+clear all, clc
+
+%using source: 
+%https://www.mathworks.com/help/optim/ug/nonlinear-curve-fitting-with-lsqcurvefit.html
+
+
+%% Initial Conditions; would be equivalent to an inducer 
+
+AA_conc = 0.001;          % AA homodimer initial (nM)
+BB_conc = 0.001;         %BB homodimer initial (nM)
+MKATE2 = 0.000;               % mKate2
+
+initialC = [AA_conc BB_conc MKATE2]; 
+
+%% Parameters
+
+nH_Est = 1.2;              % Hill for Est known from Baker
+nH_Ald = 1.2;              % Hill for Ald known from Baker
+nH_het = 1.163;              %                   ***Optimize this value??
+
+kact = 0.97;                % Transcriptional activation rate for all equations
+
+kdeg1 = 0.01;           % Degredation rate of AA
+kdeg2 = 0.01;           % Degredation rate of BB
+kdeg3 = 0.0089;         % Degredation rate of mKate       **Optimize this value
+
+%ec values, dissociation constant 
+ec501 = 64; %nM, known from Baker (Est)
+ec502 = 200; %nM, known from Baker (Ald)
+ec503 = 196; %nM, guess for now
+
+kobs = 1.25e-4;          % Exchange rate parameter from Baker; not precise  **Optimize this value
+
+%initialGuesses = [kdeg3 ec503 kobs]; 
+
+%% read in experimental data  
+simVal = readtable('intStagC.xlsx'); 
+sim330 = simVal.yAS331;
+sim330 = sim330'; 
+%simTime = (0:16); %This will have to be changed to real values
+simTime = [1 6:18 29];
+simTime = simTime.*60;
+
+%% fitting 
+%tspan = simTime'; 
+% ydata = sim330';  
+
+% fun = @(x, t) oof(tspan, initialC, initialGuesses); 
+% [B,resnorm,residual,exitflag,output,lambda,jacobian] = lsqcurvefit(fun, initialGuesses, simTime, ydata); 
+
+sim330 = 100*sim330./max(sim330);
+t=simTime';
+c=sim330';
+theta0=[kdeg3;ec503;kobs;nH_het; AA_conc; BB_conc; MKATE2];
+%theta0 = rand(8,1); 
+%lb = [0.001 100 1e-4 1.0 0 0 ]; 
+%ub = [0.10 300 1e-3 2.0 200 200 200 3.0]; 
+options = optimoptions('lsqcurvefit','OptimalityTolerance', 1e-15, 'FunctionTolerance', ...,
+    1e-16, 'StepTolerance', 1e-22, 'MaxFunctionEvaluations', 1e2, 'MaxIterations', 1e2); 
+[theta,Rsdnrm,Rsd,ExFlg,OptmInfo,Lmda,Jmat]=lsqcurvefit(@kinetics,theta0,t,c, [], [], options);
+
+%% try multi start and global search 
+% problem = createOptimProblem('lsqcurvefit', 'objective', @kinetics, 'x0', ..., 
+%      theta0, 'xdata', t, 'ydata', c, 'lb', lb, 'ub', ub, 'options', options); 
+%  gs = MultiStart; 
+%  [x,f] = run(gs, problem, 20); 
+
+%% solve ODE
+% Time Vector, need to change time vector depending on which experiment 
+tmin = 0;                    % start time
+tmax = 28*60;                 % end time (min.) 
+inc  = 28*60;                  % # of time steps
+time = linspace(tmin,tmax,inc);
+
+% Induction Vector (when Estradiol is given to cells over time course)
+ON_time = 28*60;              % Units = min.
+OFF_time = (0)*60;             % Units = min.
+Est = 100*[ones(1,ceil((ON_time/tmax)*inc)) zeros(1,floor((OFF_time/tmax)*inc))];
+
+%Induction Vector for Aldosterone
+ON_A = 4*60; %ON for aldosterone, units = min 
+OFF_A = 24*60; %OFF for aldosterone, units = min 
+Ald = 100*[zeros(1,ceil((ON_A/tmax)*inc)) ones(1,floor((OFF_A/tmax)*inc))]; %units = nM
+%% 
+[T,X] = ode45(@(t,y)ZEV_Induction_ODE_updated(t,y,time,Est,Ald,kact,kdeg1,kdeg2,theta(1),nH_Est, nH_Ald, theta(4), ec501, ec502, theta(2), theta(3)),time, initialC);
+AA_conc = X(:,1);
+BB_conc = X(:,2);
+MKATE2 = X(:,3);
+
+
+%% plotting
+
+%times = linspace(simTime(1),simTime(end));
+plot(simTime,c,'ko')
+hold on
+plot(time,MKATE2); hold on
+%times,fun(x, times) ,'b-') %this part doesnt work either 
+%legend('Data','Fitted Curve')
+%title('Data and Fitted Curve')
+
+%% function 
+
+function C = kinetics(theta,t)
+    %dcdt=zeros(3,1);
+    c0 = theta(5:7);      %[AA_conc BB_conc MKATE2]
+    [T,Cv]=ode45(@DifEq,t,c0);
+
+    function dC = DifEq(t,c)
+        dcdt=zeros(3,1);
+        
+        %kdeg3 = theta(1); %kdeg3 guess
+        %ec503 = theta(2); %ec503 guess
+        %kobs = theta(3); %kobs guess
+        
+        
+        AA_conc = c0(1); 
+        BB_conc = c0(2); 
+        MKATE2 = c0(3); 
+        
+        nH_Est = 1.2;              % Hill for Est known from Baker
+        nH_Ald = 1.2;              % Hill for Ald known from Baker
+        nH_het = 1.2;              %      ***Optimize this value??
+
+        kact = 1;                  % Transcriptional activation rate for all equations
+
+        kdeg1 = 0.01;              % Degredation rate of AA
+        kdeg2 = 0.01; 
+        
+        ec501 = 64; %nM, known from Baker (Est)
+        ec502 = 200; %nM, known from Baker (Ald)
+        
+        tmin = 0;                    % start time
+        tmax = 28*60;                 % end time (min.)
+        inc  = 28*60;                  % # of time steps
+        time = linspace(tmin,tmax,inc); 
+        
+        ON_time = 28*60;              % Units = min.
+        OFF_time = 0;             % Units = min.
+        Est  = 100*[ones(1,(ON_time/tmax)*inc) zeros(1,(OFF_time/tmax)*inc)];
+        
+        ON_A = 4*60; %ON for aldosterone, units = min 
+        OFF_A = 24*60; %OFF for aldosterone, units = min 
+        Ald = 100*[ones(1,(ON_A/tmax)*inc) zeros(1,(OFF_A/tmax)*inc)]; %units = nM
+               
+        EstI = interp1(time,Est,t)';
+        AldI = interp1(time,Ald,t)';
+        
+        dcdt(1) = kact.*[EstI].^nH_Est./([EstI].^nH_Est + ec501.^nH_Est) - kdeg1.*c(1);
+        dcdt(2) = kact.*[AldI].^nH_Ald./([AldI].^nH_Ald + ec502.^nH_Ald) - kdeg2.*c(2);
+        dcdt(3) = kact.*[c(1).*c(2)./(theta(3)).^0.5]^theta(4)./([c(1).*c(2)./(theta(3)).^0.5].^theta(4) + theta(2).^nH_het) - theta(1).*c(3);
+        dC=dcdt;
+    end
+C=Cv(:,3);
+C(find(isnan(C))) = 0;
+
+end
